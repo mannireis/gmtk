504ab1dfda3b86623c1cf163bbdbd11dbf81b924
mannireis <90857553+mannireis@users.noreply.github.com>
2026-07-23 17:31:50 +0100

    Make the player controller base with
    - Accel + deaccel
    - Jumping + Coyote Jump + Jump Buffer
    - A very simple test level with nothing on it

---
+++ scripts/plaer/main.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_buffer_timer: Timer = $JumpBufferTimer
+
+const jump_height: float = -230.0
+const max_gravity: float = 14.5
+const max_speed: float = 120.0
+const accelaration: float = 8.0
+const friction: float = 10.0
+
+var coyote_time_activated: bool = false
+
+var gravity: float = 12.0
+
+
+func _physics_process(delta: float) -> void:
+	var x_input: float = Input.get_axis("Left", "Right")
+	var velocity_weight: float = delta * (accelaration if x_input else friction)
+	velocity.x = lerp(velocity.x, x_input * max_speed, velocity_weight)
+	
+	if is_on_floor():
+		coyote_time_activated = false
+		gravity = lerp(gravity, 12.8, 12.0 * delta)
+	else:
+		if coyote_timer.is_stopped() and !coyote_time_activated:
+			coyote_timer.start()
+			coyote_time_activated = true
+		
+		if Input.is_action_just_released("Jump") or is_on_ceiling():
+			velocity.y *= 0.5
+		
+		gravity = lerp(gravity, max_gravity, 12.3 * delta)
+
+	if Input.is_action_just_pressed("Jump"):
+		if jump_buffer_timer.is_stopped():
+			jump_buffer_timer.start()
+		if not jump_buffer_timer.is_stopped() and (not coyote_timer.is_stopped()) or is_on_floor():
+			velocity.y = jump_height
+			jump_buffer_timer.stop()
+			coyote_timer.stop()
+			coyote_time_activated = true
+	
+	velocity.y += gravity
+	
+	move_and_slide()
